05e3338101f5d316b0a93769a5605c5f181b1457
Christiaan de Wet <christiaan.dewet@living-stratus.com>
2017-11-27 20:47:14 +0200

    added a try/except banning method, this is actually not a good solution. Take three

--- core/handler.py
+++ core/handler.py
@@ -115,7 +115,7 @@ def update_ban_list(messages):
         messages_count = dict(Counter([item['message']['from']['first_name'] for item in messages['result']]))
     except KeyError:
         logger.debug(f'Oh shit, lets hope this does not happen often. No banning can occur this time around.')
-        handle_messages(messages)
+        return
     # TODO: There has to be a better way, this is a shitty hack.
 
     '''
